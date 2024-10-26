333be0e17ab9a4ecdcd2bda6b7505d8ef82dcd06
redmi4ek <radmirsolo@gmail.com>
2024-10-26 13:30:59 +0500

    nativewand and some dependences

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-300" >
+      <Text className="text-3xl font-pbold">Nope i wont</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Go to profile</Link>
+    </View>
+  );
+}
